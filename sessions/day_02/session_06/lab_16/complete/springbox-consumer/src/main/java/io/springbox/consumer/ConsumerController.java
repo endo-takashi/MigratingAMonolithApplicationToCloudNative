31b3c7d857ba311a9ede06a521ddb3e285abe86e
Carlos Queiroz <cqueiroz@gopivotal.com>
2015-11-17 14:24:59 +0530

    Deletes not required gradle files.

--- sessions/day_02/session_06/lab_16/complete/springbox-consumer/src/main/java/io/springbox/consumer/ConsumerController.java
+++ sessions/day_02/session_06/lab_16/complete/springbox-consumer/src/main/java/io/springbox/consumer/ConsumerController.java
@@ -1,7 +1,5 @@
 package io.springbox.consumer;
 
-import com.netflix.appinfo.InstanceInfo;
-import com.netflix.discovery.DiscoveryClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,15 +8,17 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class ConsumerController {
 
+
+
     @Autowired
-    DiscoveryClient discoveryClient;
+    RestTemplate restTemplate;
 
     @RequestMapping(value = "/", produces = "application/json")
     String consume() {
-        InstanceInfo instance = discoveryClient.getNextServerFromEureka("PRODUCER", false);
+//        InstanceInfo instance = discoveryClient.getNextServerFromEureka("PRODUCER", false);
 
-        RestTemplate restTemplate = new RestTemplate();
-        ProducerResponse response = restTemplate.getForObject(instance.getHomePageUrl(), ProducerResponse.class);
+//        RestTemplate restTemplate = new RestTemplate();
+        ProducerResponse response = restTemplate.getForObject("http://producer", ProducerResponse.class);
 
         return String.format("{\"value\":%d}", response.getValue());
     }
