91a1aa4b99e3d9a33abf710bc8f9b550d5611226
Carlos Queiroz <cqueiroz@gopivotal.com>
2016-01-13 15:47:17 +0800

    Adds lab 17 (session 07).

---
+++ sessions/day_02/session_07/lab_17/complete/springtrader-consumer/src/main/java/io/springtrader/consumer/SpringtraderConsumerApplication.java
@@ -0,0 +1,16 @@
+package io.springtrader.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RibbonClient(name = "producer", configuration = ProducerConfiguration.class)
+public class SpringtraderConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringtraderConsumerApplication.class, args);
+    }
+}
