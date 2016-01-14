f1ae82ad2fb03cdd23153bd52c82824e6acb8d89
Carlos Queiroz <cqueiroz@gopivotal.com>
2016-01-15 03:39:20 +0800

    Modifies lab 19 (session 07). springtrader.

---
+++ sessions/day_02/session_07/lab_19/initial/springtrader-consumer/src/main/java/io/springtrader/consumer/SpringtraderConsumerApplication.java
@@ -0,0 +1,15 @@
+package io.springtrader.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringtraderConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringtraderConsumerApplication.class, args);
+    }
+}
