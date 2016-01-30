b9d08b9b91398750d4841f752168fcab4d0d9e5d
Paul Chapman <chapman@zemsys.com>
2016-01-30 13:21:59 +1100

    Tidy formatting

--- sessions/day_01/session_05/lab_12/complete/springtrader-accounts/src/main/java/io/pivotal/springtrader/accounts/domain/Account.java
+++ sessions/day_01/session_05/lab_12/complete/springtrader-accounts/src/main/java/io/pivotal/springtrader/accounts/domain/Account.java
@@ -1,12 +1,22 @@
 package io.pivotal.springtrader.accounts.domain;
 
-import org.springframework.format.annotation.DateTimeFormat;
-
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+
 
 /**
  * Represents the account.
